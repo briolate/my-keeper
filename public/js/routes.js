2c0a752859b22bfd939a78985f551c54a304f4d8
Evan Briolat <briolate@gmail.com>
2017-08-30 17:36:17 -0400

    Fixed routing

--- public/js/routes.js
+++ public/js/routes.js
@@ -1,12 +1,36 @@
-var app = angular.module('draftApp', ['ngRoute']);
-app.config(function($routeProvider) {
-  $routeProvider.when('/home', {
-    templateUrl: 'views/home.html'
-  }).when('/draftOrder', {
-    templateUrl: 'views/draftOrder.html'
-  }).when('/myKeeper', {
-    templateUrl: 'views/myKeeper.html'
-  }).when('/rankings', {
-    templateUrl: 'views/rankings.html'
-  }).otherwise('/home');
+var app = angular.module('draftApp', ['ui.router']);
+app.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: 'views/home.html'
+    }).state('draftOrder', {
+      url: '/draftOrder',
+      templateUrl: 'views/draftOrder.html'
+    }).state('myKeeper', {
+      url: '/myKeeper',
+      templateUrl: 'views/myKeeper.html'
+    }).state('rankings', {
+      url: '/rankings',
+      templateUrl: 'views/rankings.html'
+    }).state('rankings.qb', {
+      url: '/qb',
+      templateUrl: 'views/rankings/qb.html'
+    }).state('rankings.rb', {
+      url: '/rb',
+      templateUrl: 'views/rankings/rb.html'
+    }).state('rankings.wr', {
+      url: '/wr',
+      templateUrl: 'views/rankings/wr.html'
+    }).state('rankings.te', {
+      url: '/te',
+      templateUrl: 'views/rankings/te.html'
+    }).state('rankings.dst', {
+      url: '/dst',
+      templateUrl: 'views/rankings/dst.html'
+    }).state('rankings.k', {
+      url: '/k',
+      templateUrl: 'views/rankings/k.html'
+    });
+  $urlRouterProvider.otherwise('home');
 });
